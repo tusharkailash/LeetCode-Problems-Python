a47ec078ea1c0e5723384b8df90d6b88c7dee081
tusharkailash <tusharkailash29@gmail.com>
2019-11-05 23:41:04 -0800

    Design HashMap

---
+++ Amazon/designHashMap.py
@@ -0,0 +1,64 @@
+# Design a HashMap without using any built-in hash table libraries.
+#
+# To be specific, your design should include these functions:
+#
+# put(key, value) : Insert a (key, value) pair into the HashMap. If the value already exists in the HashMap, update the value.
+# get(key): Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+# remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.
+#
+# MyHashMap hashMap = new MyHashMap();
+# hashMap.put(1, 1);
+# hashMap.put(2, 2);
+# hashMap.get(1);            // returns 1
+# hashMap.get(3);            // returns -1 (not found)
+# hashMap.put(2, 1);          // update the existing value
+# hashMap.get(2);            // returns 1
+# hashMap.remove(2);          // remove the mapping for 2
+# hashMap.get(2);            // returns -1 (not found) 
+
+class MyHashMap(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.dict = {}
+
+    def put(self, key, value):
+        """
+        value will always be non-negative.
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        self.dict[key] = value
+
+    def get(self, key):
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        :type key: int
+        :rtype: int
+        """
+        if key not in self.dict:
+            return -1
+        else:
+            return self.dict[key]
+
+    def remove(self, key):
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        :type key: int
+        :rtype: None
+        """
+        if key in self.dict:
+            del self.dict[key]
+        else:
+            return -1
+
+
+obj = MyHashMap()
+obj.put(2,3)
+param_2 = obj.get(2)
+print param_2
+obj.remove(2)
+obj.get(2)
